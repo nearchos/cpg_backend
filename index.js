6ee5ef71c4f1926b3b7a6f2a7c8f79b25c094800
Nearchos Paspallis <nearchos@aspectsense.com>
2022-06-21 15:46:29 +0300

    handling magic

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/', (req, res) => { // handle GET requests at '/'
     res.send(`Hello World! -> authorized: ${authorized}, from: ${from}`);
 })
 
+app.get('/sync', (req, res) => { // handle GET requests at '/sync'
+    const magic = req.query.magic;
+    const from = req.query.from;
+    let authorized = magic === process.env.magic;
+    res.send(`magic: ${magic} -> authorized: ${authorized}, from: ${from}`);
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
