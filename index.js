a36023e3b30109264da1a0d2f5f0bd62c1c50d7f
Nearchos Paspallis <nearchos@aspectsense.com>
2022-06-21 15:38:46 +0300

    handling magic

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express(); // init express
 
-app.get('/', (req, res) => { // handle GET requests at '/’
-    res.send('Hello World!') // simply send back 'Hello World'
+app.get('/', (req, res) => { // handle GET requests at '/'
+    let from = req.params['from'];
+    let authorized = req.params['magic'] === process.env.magic;
+    res.send(`Hello World! -> authorized: ${authorized}, from: ${from}`);
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
